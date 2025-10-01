e1a913009ff5ce6e7c5ba500284e9e33284b66a4
Cashh13 <152657924+Cashh13@users.noreply.github.com>
2025-10-01 17:51:28 -0400

    Fixed increasing speed bug

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,35 +6,22 @@ import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
 
+public class GamePanel extends JFrame implements KeyListener {
 
-public class GamePanel extends JFrame implements KeyListener{
+    private boolean zig = false;
 
     JLabel label;
     JButton start;
     JLabel scoreLabel;
     JLabel highScoreLabel;
-    //JLabel bg = new JLabel(new ImageIcon("space.png"));
     private int speed = 350;
     JLabel gameMode;
 
-
-
-
     Random random = new Random();
-    private boolean w;
-    private boolean d;
-    private boolean s;
-    private boolean a;
-    private boolean w2;
-    private boolean d2;
-    private boolean s2;
-    private boolean a2;
-    private boolean running = false;
+    private boolean w, d, s, a;
     private int score;
     private int playAgain;
     private int highScore;
-
-    int gitTest;
 
     private Timer timer;
     private Timer timer2;
@@ -43,74 +30,59 @@ public class GamePanel extends JFrame implements KeyListener{
     private JRadioButton easyButton;
     private JRadioButton mediumButton;
     private JRadioButton hardButton;
-
+    private JRadioButton expertButton;
     private ButtonGroup modeGroup;
 
     private List<Obstacle> obstacles = new ArrayList<>();
 
-
-
-
-
-
-    GamePanel(){
+    GamePanel() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 800);
         this.setLayout(null);
-        //bg.setLayout(new BorderLayout());
         this.addKeyListener(this);
         start = new JButton("Start");
         start.setBackground(Color.white);
         this.setResizable(false);
+
         modeGroup = new ButtonGroup();
         gameMode = new JLabel("Game Mode");
-
 
         easyButton = new JRadioButton("Easy");
         mediumButton = new JRadioButton("Medium");
         hardButton = new JRadioButton("Hard");
+        expertButton = new JRadioButton("Expert");
 
-        // Set bounds (position and size)
         easyButton.setBounds(360, 260, 80, 25);
         mediumButton.setBounds(360, 280, 80, 25);
         hardButton.setBounds(360, 300, 80, 25);
-
-
+        expertButton.setBounds(360, 320, 80, 25);
 
         gameMode.setForeground(Color.WHITE);
         gameMode.setFont(new Font("Arial", Font.BOLD, 20));
-
         gameMode.setBounds(345,200,250,50);
 
         this.add(easyButton);
         this.add(mediumButton);
         this.add(hardButton);
+        this.add(expertButton);
         this.add(gameMode);
 
         modeGroup.add(easyButton);
         modeGroup.add(mediumButton);
         modeGroup.add(hardButton);
+        modeGroup.add(expertButton);
 
         mediumButton.setSelected(true);
 
-
-
-        //setContentPane(bg);
         int y = this.getHeight()/2 - 25;
         int x = this.getWidth() /2 - 75;
 
         start.setFont(new Font("Arial", Font.BOLD, 20));
         start.setBorder(BorderFactory.createLineBorder(Color.CYAN));
         start.setFocusPainted(false);
-
-
         start.setBounds(x,y,150,50);
-
         start.setForeground(Color.BLACK);
-
         this.add(start);
-
-
 
         scoreLabel = new JLabel("Score: " + score);
         scoreLabel.setForeground(Color.WHITE);
@@ -118,190 +90,121 @@ public class GamePanel extends JFrame implements KeyListener{
         scoreLabel.setBounds(10, 10, 200, 30);
         this.add(scoreLabel);
 
-
         highScoreLabel = new JLabel("High Score: " + highScore);
         highScoreLabel.setForeground(Color.WHITE);
         highScoreLabel.setFont(new Font("Arial", Font.BOLD, 20));
         highScoreLabel.setBounds(625, 10, 200, 30);
         this.add(highScoreLabel);
-        GamePanel.this.repaint();
 
+        getContentPane().setBackground(Color.BLACK);
 
-        ActionListener actionListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (easyButton.isSelected()) {
-
-                    speed = 1000;
-                    gameMode.setBounds(365,200,250,50);
-                    gameMode.setText("Easy");
-
-                } else if (mediumButton.isSelected()) {
-
-                    speed = 350;
-                    gameMode.setBounds(365,200,250,50);
-                    gameMode.setText("Medium");
-
-                } else if (hardButton.isSelected()) {
-
-                    speed = 200;
-                    gameMode.setBounds(365,200,250,50);
-                    gameMode.setText("Hard");
-
-
-                }
-
-
-                if (timer2 != null) {
-                    timer2.stop();
-                }
-
-
-                timer2 = new Timer(speed, new ActionListener() { //speed
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        Obstacle o = createObstacle();
-                        obstacles.add(o);
-                        GamePanel.this.add(o.getLabel());
-                        GamePanel.this.repaint();
-                    }
-                });
-
-
-
-
-
+        // Difficulty selection
+        ActionListener actionListener = e -> {
+            if (easyButton.isSelected()) {
+                speed = 1000;
+                gameMode.setText("   Easy");
+            } else if (mediumButton.isSelected()) {
+                speed = 350;
+                gameMode.setText("   Medium");
+            } else if (hardButton.isSelected()) {
+                speed = 200;
+                gameMode.setText("   Hard");
+            } else if (expertButton.isSelected()) {
+                speed = 200;
+                gameMode.setText("   Expert");
             }
 
+
+            timer2.setDelay(speed);
         };
         easyButton.addActionListener(actionListener);
         mediumButton.addActionListener(actionListener);
         hardButton.addActionListener(actionListener);
+        expertButton.addActionListener(actionListener);
 
-        this.add(easyButton);
-        this.add(mediumButton);
-        this.add(hardButton);
-
-
-        start.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-
-                GamePanel.this.remove(start);
-                GamePanel.this.remove(gameMode);
-                GamePanel.this.remove(easyButton);
-                GamePanel.this.remove(mediumButton);
-                GamePanel.this.remove(hardButton);
-                GamePanel.this.repaint();
-                timer2.start();
-                timer3.start();
-                GamePanel.this.requestFocusInWindow();
-            }
+        start.addActionListener(e -> {
+            GamePanel.this.remove(start);
+            GamePanel.this.remove(gameMode);
+            GamePanel.this.remove(easyButton);
+            GamePanel.this.remove(mediumButton);
+            GamePanel.this.remove(hardButton);
+            GamePanel.this.remove(expertButton);
+            GamePanel.this.repaint();
+            timer2.start();
+            timer3.start();
+            GamePanel.this.requestFocusInWindow();
         });
-
-
-
-
-
-
-
-
-
 
         label = new JLabel();
         label.setBounds(350, 500, 100, 175);
-
-        ImageIcon rocket1 = new ImageIcon("rocket.png");
+        ImageIcon rocket1 = new ImageIcon(getClass().getResource("/rocket.png"));
         Image rocket2 = rocket1.getImage();
         Image rocket3 = rocket2.getScaledInstance(100, 175, Image.SCALE_SMOOTH);
         rocket = new ImageIcon(rocket3);
-
-        getContentPane().setBackground(Color.BLACK);
-
-
-
         label.setIcon(rocket);
-
-
         this.add(label);
         this.setVisible(true);
 
-
-
-
-        timer = new Timer(1, new ActionListener() { //delay for movement
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                move();
-                inFrame();
-                crash();
-
-
-            }
+        // Timers
+        timer = new Timer(1, e -> {
+            move();
+            inFrame();
+            crash();
         });
-
         timer.start();
 
-
-        timer2 = new Timer(speed, new ActionListener() {  //how often stars drop
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                Obstacle o = createObstacle();
-                obstacles.add(o);
-                GamePanel.this.add(o.getLabel());
-                GamePanel.this.repaint();
-
-
-            }
+        timer2 = new Timer(speed, e -> {
+            Obstacle o = createObstacle();
+            obstacles.add(o);
+            GamePanel.this.add(o.getLabel());
+            GamePanel.this.repaint();
         });
 
-
-
-        timer3 = new Timer(1000, new ActionListener() { //score
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                score++;
-                if (score > highScore) {
-                    highScore++;
-                }
-
-                highScoreLabel.setText("High Score: " + highScore);
-                scoreLabel.setText("Score: " + score);
-                GamePanel.this.repaint();
-
+        timer3 = new Timer(1000, e -> {
+            score++;
+            if (score > highScore) {
+                highScore = score;
             }
+            highScoreLabel.setText("High Score: " + highScore);
+            scoreLabel.setText("Score: " + score);
+            GamePanel.this.repaint();
         });
-
-
-
-
     }
 
     private void crash() {
         Iterator<Obstacle> iterator = obstacles.iterator();
         while (iterator.hasNext()) {
+
+
+
             Obstacle o = iterator.next();
+
+
+
+
             o.moveDown(5);
             if (o.getBounds().intersects(label.getBounds())) {
                 stopGame();
                 return;
             }
             if (o.getBounds().getY() > this.getHeight()) {
+                o.stop(); // stop obstacle timer
                 this.remove(o.getLabel());
                 iterator.remove();
             }
         }
     }
 
-
-
-
-
     public Obstacle createObstacle(){
-        return new Obstacle(getrandX(), -10, 40, 40);
-    }
 
+
+
+
+        return new Obstacle(getrandX(), -10, 40, 40);
+
+
+
+    }
 
     private void stopGame() {
         label.setLocation(350, 500);
@@ -312,37 +215,19 @@ public class GamePanel extends JFrame implements KeyListener{
         restartGame();
     }
 
-
-
     private void restartGame() {
         if (playAgain == JOptionPane.OK_OPTION) {
             score = 0;
-
-            if (score > highScore)
-            {
-                highScore = score;
-            }
-
             highScoreLabel.setText("High Score: " + highScore);
             scoreLabel.setText("Score: " + score);
 
-
-
             for (Obstacle o : obstacles) {
+                o.stop(); // stop timers of old obstacles
                 this.remove(o.getLabel());
             }
             obstacles.clear();
-            w = false;
-            d = false;
-            a = false;
-            s = false;
 
-            //arrow keys
-            w2 = false;
-            d2 = false;
-            a2 = false;
-            s2 = false;
-
+            w = d = s = a = false;
             this.repaint();
 
             timer.start();
@@ -352,173 +237,59 @@ public class GamePanel extends JFrame implements KeyListener{
             System.exit(0);
         }
     }
-    //test
-
 
     public int getrandX(){
         return random.nextInt(801);
     }
 
-//    public int getrandY(){
-//        return random.nextInt(-10);
-//    }
 
     @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
+    public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
-        w2 = e.getKeyCode() == 38;
-      a2 = e.getKeyCode() == 37;
-      s2 = e.getKeyCode() == 40;
-      d2 = e.getKeyCode() == 39;
-
         switch (e.getKeyChar()) {
-
-            case 'w':
-
-                w = true;
-                break;
-            case 'a':
-
-                a = true;
-                break;
-            case 's':
-
-                s = true;
-                break;
-            case 'd':
-
-                d = true;
-                break;
-        }
-
-
-            //Arrow keys
-            switch (e.getKeyCode()){
-            case 38:
-                w2 = true;
-                break;
-            case 37:
-                a2 = true;
-                break;
-            case 40:
-                s2 = true;
-                break;
-            case 39:
-                d2 = true;
-                break;
-
-
-
+            case 'w': w = true; break;
+            case 'a': a = true; break;
+            case 's': s = true; break;
+            case 'd': d = true; break;
         }
         move();
         inFrame();
-
-
     }
-
-
-
-
-    public void inFrame(){
-        int x = label.getX();
-        int y = label.getY();
-
-
-        if (x < 0) {
-            x=0;
-        }
-        else if (x +label.getWidth() > this.getWidth()) {
-            x = this.getWidth() - label.getWidth();
-        }
-
-        if (y < 0) {
-            y=0;
-        }
-        else if (y +label.getHeight() > this.getHeight()) {
-            y = this.getHeight() - label.getHeight();
-        }
-
-        label.setLocation(x, y);
-
-
-    }
-
-    public void move() {
-
-
-        if (w && d || w2 && d2) {
-            label.setLocation(label.getX() + 7, label.getY() - 7);
-        } else if (a && w || a2 && w2) {
-            label.setLocation(label.getX() - 7, label.getY() - 7);
-        } else if (s && a || s2 && a2) {
-            label.setLocation(label.getX() - 7, label.getY() + 7);
-        } else if (s && d || s2 && d2) {
-            label.setLocation(label.getX() + 7, label.getY() + 7);
-        } else {
-
-            if (w || w2) {
-                label.setLocation(label.getX(), label.getY() - 7); // Moving up
-            }
-            if (a || a2) {
-                label.setLocation(label.getX() - 7, label.getY()); // Moving left
-            }
-            if (s || s2) {
-                label.setLocation(label.getX(), label.getY() + 7); // Moving down
-            }
-            if (d || d2) {
-                label.setLocation(label.getX() + 7, label.getY()); // Moving right
-            }
-
-        }
-
-
-    }
-
-
 
     @Override
     public void keyReleased(KeyEvent e) {
         switch(e.getKeyChar()) {
-
-            case 'w':
-                w=false;
-                break;
-            case 'a':
-                a=false;
-                break;
-            case 's':
-                s=false;
-                break;
-            case 'd':
-                d=false;
-                break;
-        }
-        switch(e.getKeyCode()) {
-
-            case 38:
-                w2=false;
-                break;
-            case 37:
-                a2=false;
-                break;
-            case 40:
-                s2=false;
-                break;
-            case 39:
-                d2=false;
-                break;
+            case 'w': w=false; break;
+            case 'a': a=false; break;
+            case 's': s=false; break;
+            case 'd': d=false; break;
         }
         move();
         inFrame();
-
     }
 
+    public void inFrame(){
+        int x = label.getX();
+        int y = label.getY();
+        if (x < 0) x=0;
+        else if (x +label.getWidth() > this.getWidth()) x = this.getWidth() - label.getWidth();
+        if (y < 0) y=0;
+        else if (y +label.getHeight() > this.getHeight()) y = this.getHeight() - label.getHeight();
+        label.setLocation(x, y);
+    }
 
-
-
-
+    public void move() {
+        if (w && d) label.setLocation(label.getX() + 7, label.getY() - 7);
+        else if (a && w) label.setLocation(label.getX() - 7, label.getY() - 7);
+        else if (s && a) label.setLocation(label.getX() - 7, label.getY() + 7);
+        else if (s && d) label.setLocation(label.getX() + 7, label.getY() + 7);
+        else {
+            if (w) label.setLocation(label.getX(), label.getY() - 7);
+            if (a) label.setLocation(label.getX() - 7, label.getY());
+            if (s) label.setLocation(label.getX(), label.getY() + 7);
+            if (d) label.setLocation(label.getX() + 7, label.getY());
+        }
+    }
 }
--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,23 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Obstacle{
-
+public class Obstacle {
 
     private Timer t;
     ImageIcon star;
     JLabel label;
     Rectangle s;
-    int speed;
+
 
 
     public Obstacle(int x, int y, int width, int height) {
-
-
-
-        ImageIcon star1 = new ImageIcon("star.png");
+        ImageIcon star1 = new ImageIcon(getClass().getResource("/star.png"));
         Image star2 = star1.getImage();
         Image star3 = star2.getScaledInstance(60, 60, Image.SCALE_SMOOTH);
         star = new ImageIcon(star3);
@@ -26,33 +20,28 @@ public class Obstacle{
         label.setBounds(x, y, width, height);
         s = label.getBounds();
 
-
-//        label.setIcon(star);
-
-        t = new Timer(10, e -> moveDown(5)); //speed of stars
+        // obstacle moves on its own timer
+        t = new Timer(10, e -> moveDown(5));
         t.start();
-
-
     }
 
     public void moveDown(int speed) {
         label.setLocation(label.getX(), label.getY() + speed);
-        s=label.getBounds();
+        s = label.getBounds();
     }
 
-    public JLabel getLabel() {
 
+    public JLabel getLabel() {
         return label;
     }
 
     public Rectangle getBounds() {
         return label.getBounds();
     }
-    public void removeAll() {
 
-        this.removeAll();
+    public void stop() {
+        if (t != null) {
+            t.stop();
+        }
     }
-
-
 }
-
